dc037efe540dc41314e2bf9ebf17b0216ae30c42
Michael Riley <michaelriley@situsamc.com>
2023-04-09 16:40:02 -0600

    fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
 
+  import './app.css';
+	import 'iconify-icon';
+
+	const meta = {
+		title: 'PreSveltation: Create Presentations and Slideshows with Svelte & Tailwind CSS',
+		description:
+			'Create interactive and dynamic presentations and slideshows with Svelte / SvelteKit and Tailwind CSS.'
+	};
+
+</script>
 <slot />
